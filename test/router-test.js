2c1174bb3260231a7c079ee7baa0da24507af76f
Ganesha <reekoheek@gmail.com>
2016-03-31 13:08:52 +0700

    REFACTORED route signature accept single route object

--- test/router-test.js
+++ test/router-test.js
@@ -1,11 +1,60 @@
-var assert = require('assert');
-
-var router = require('../lib/router');
+const assert = require('assert');
+const router = require('../lib/router');
 
 describe('Router', function() {
   'use strict';
 
-  var r = router();
+  var r;
+
+  beforeEach(function() {
+    r = router();
+  });
+
+  describe('#_isStatic', function() {
+    it('return true if no regexp', function() {
+      assert(r._isStatic('/test'));
+      assert(r._isStatic('/foo/bar'));
+    });
+
+    it('return false if regexp', function() {
+      assert.equal(r._isStatic('/[test]'), false);
+      assert.equal(r._isStatic('/foo/{bar}'), false);
+      assert.equal(r._isStatic('/foo[/{bar}]'), false);
+    });
+  });
+
+  describe('#route', function() {
+    beforeEach(function() {
+      r = router();
+    });
+
+    it('return own instance and can be chained', function() {
+      var result = r.route({
+        methods: ['GET'],
+        pattern: '/test',
+      });
+      assert.equal(result, r);
+      assert.equal(r.routes.GET[0].type, 's');
+    });
+
+    it('able to create static, variable, and regex routes', function() {
+      r.route({
+          methods: ['GET'],
+          pattern: '/test',
+        })
+        .route({
+          methods: ['POST'],
+          pattern: '/foo/{bar}'
+        })
+        .route({
+          methods: ['PUT'],
+          pattern: /^\/some\/regex\/(\d+)/
+        });
+      assert.equal(r.routes.GET[0].type, 's');
+      assert.equal(r.routes.POST[0].type, 'v');
+      assert.equal(r.routes.PUT[0].type, 'v');
+    });
+  });
 
   describe('#_routeRegExp', function() {
     it('should handle optional ([])', function() {
@@ -14,12 +63,15 @@ describe('Router', function() {
       str = '/foo/bar[/baz]';
       expected = /^\/foo\/bar(?:\/baz)?$/.toString();
       re = r._routeRegExp(str);
-      assert.equal(re.toString(), expected);
+
+      assert.equal(re[0].toString(), expected);
+      assert.equal(re[1].length, 0);
 
       str = '/foo[/bar/baz]';
       expected = /^\/foo(?:\/bar\/baz)?$/.toString();
       re = r._routeRegExp(str);
-      assert.equal(re.toString(), expected);
+      assert.equal(re[0].toString(), expected);
+      assert.equal(re[1].length, 0);
     });
 
     it('should handle params ({})', function() {
@@ -28,7 +80,8 @@ describe('Router', function() {
       str = '/foo/{bar}/{baz}';
       expected = /^\/foo\/([^\/]+)\/([^\/]+)$/.toString();
       re = r._routeRegExp(str);
-      assert.equal(re.toString(), expected);
+      assert.equal(re[0].toString(), expected);
+      assert.deepEqual(re[1], ['bar', 'baz']);
     });
 
     it('should handle both optionals ([]) and params ({})', function() {
@@ -37,7 +90,8 @@ describe('Router', function() {
       str = '/foo/{bar}[/{baz}]';
       expected = /^\/foo\/([^\/]+)(?:\/([^\/]+))?$/.toString();
       re = r._routeRegExp(str);
-      assert.equal(re.toString(), expected);
+      assert.equal(re[0].toString(), expected);
+      assert.deepEqual(re[1], ['bar', 'baz']);
     });
   });
 });
