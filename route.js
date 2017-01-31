4895783abdaa9b9417f6987856f8453089ba2fbb
Ganesha <reekoheek@gmail.com>
2017-01-31 20:51:20 +0700

    implemented dynamic routing

--- route.js
+++ route.js
@@ -1,8 +1,35 @@
 class Route {
+  static routeRegExp (str) {
+    let chunks = str.split('[');
+
+    if (chunks.length > 2) {
+      throw new Error('Invalid use of optional params');
+    }
+
+    let tokens = [];
+    let re = chunks[0].replace(/{([^}]+)}/g, function (g, token) {
+      tokens.push(token);
+      return '([^/]+)';
+    }).replace(/\//g, '\\/');
+
+    let optRe = '';
+
+    if (chunks[1]) {
+      optRe = '(?:' + chunks[1].slice(0, -1).replace(/{([^}]+)}/g, function (g, token) {
+        tokens.push(token);
+        return '([^/]+)';
+      }).replace(/\//g, '\\/') + ')?';
+    }
+    return [ new RegExp('^' + re + optRe + '$'), tokens ];
+  }
+
   constructor (uri, callback) {
     this.uri = uri;
     this.callback = callback;
     this.isStatic = !uri.match(/[[{]/);
+    if (!this.isStatic) {
+      [ this.pattern, this.args ] = Route.routeRegExp(this.uri);
+    }
   }
 
   match (ctx) {
@@ -11,7 +38,14 @@ class Route {
         return true;
       }
     } else {
-      throw new Error('Unimplemented yet');
+      const result = ctx.url.match(this.pattern);
+      if (result) {
+        ctx.parameters = this.args.reduce((args, name, index) => {
+          args[name] = result[index + 1];
+          return args;
+        }, {});
+        return true;
+      }
     }
 
     return false;
