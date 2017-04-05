e56f52cafacf37ea117fde50065c4267d436d64f
Ganesha <reekoheek@gmail.com>
2017-04-05 15:48:30 +0700

    json middleware result body when error caught

--- middlewares/json.js
+++ middlewares/json.js
@@ -1,9 +1,17 @@
 module.exports = () => {
   return async (ctx, next) => {
-    await next();
-
-    if (ctx.state.result) {
-      ctx.body = ctx.state.result;
+    try {
+      await next();
+      if (ctx.state.result) {
+        ctx.body = ctx.state.result;
+      }
+    } catch (err) {
+      ctx.status = err.status || 500;
+      ctx.body = {
+        errors: [
+          { message: err.message, stack: err.stack },
+        ],
+      };
     }
   };
 };
